e938f4c6ca68d397181263012ffb229c00cdcfdb
acanacar <ahmetcanacar.1@gmail.com>
2019-03-19 23:38:59 +0300

    RNN is started for turkish Bank stock prices prediction

---
+++ Recurral-Neural-Network/v1.py
@@ -0,0 +1,28 @@
+# Recurrent Neural Network
+
+
+
+# Part 1 - Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the training set
+dataset_train = pd.read_csv('stockBankA_train.csv')
+training_set = dataset_train.iloc[:, 1:2].values
+
+# Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+training_set_scaled = sc.fit_transform(training_set)
+
+# Creating a data structure with 60 timesteps and 1 output
+X_train = []
+y_train = []
+for i in range(480, 27898):
+    X_train.append(training_set_scaled[i-480:i, 0])
+    y_train.append(training_set_scaled[i, 0])
+X_train, y_train = np.array(X_train), np.array(y_train)
+
