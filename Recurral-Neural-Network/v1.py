968b8a4b6707b7e9663f63e748bd1ea9fa4822dd
acanacar <ahmetcanacar.1@gmail.com>
2019-03-19 23:46:41 +0300

    Rnn is designed and plotted results with matplotlib

--- Recurral-Neural-Network/v1.py
+++ Recurral-Neural-Network/v1.py
@@ -36,3 +36,61 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Dropout
+
+# Initialising the RNN
+regressor = Sequential()
+
+# Adding the first LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+regressor.add(Dropout(0.2))
+
+# Adding a second LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+# Adding a third LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+# Adding a fourth LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 50))
+regressor.add(Dropout(0.2))
+
+# Adding the output layer
+regressor.add(Dense(units = 1))
+
+# Compiling the RNN
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+# Fitting the RNN to the Training set
+regressor.fit(X_train, y_train, epochs = 20, batch_size = 32)
+
+
+
+# Part 3 - Making the predictions and visualising the results
+
+# Getting the real stock price of 2017
+dataset_test = pd.read_csv('BankA_train.csv')
+real_stock_price = dataset_test.iloc[:, 1:2].values
+
+# Getting the predicted stock price of 2017
+dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis = 0)
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 480:].values
+inputs = inputs.reshape(-1,1)
+inputs = sc.transform(inputs)
+X_test = []
+for i in range(480, 640):
+    X_test.append(inputs[i-480:i, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+# Visualising the results
+plt.plot(real_stock_price, color = 'red', label = 'Real BankA Stock Price')
+plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted BankA Stock Price')
+plt.title('BankA Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('BankA Stock Price')
+plt.legend()
+plt.show()
