435773a35d2fb0428b5695cff574ffc88e515dde
acanacar <ahmetcanacar.1@gmail.com>
2019-04-13 22:26:16 +0300

    scrolled Text and button interaction example is created

--- Tkinter/v1.py
+++ Tkinter/v1.py
@@ -37,21 +37,59 @@ combo.grid(column=0, row=1)
 # check button
 chk_state = BooleanVar()
 chk_state.set(True)  # set check state
+
+# chk_state = IntVar()
 # chk_state.set(1)  # set check state
 # chk_state.set(0)  # set check state
 chk = Checkbutton(window, text='Choose')
 chk.grid(column=1, row=1)
 
 # radio buttons widgets
-
 rad1 = Radiobutton(window, text='First', value=1)
 rad2 = Radiobutton(window, text='Second', value=2)
 rad3 = Radiobutton(window, text='Third', value=3)
 
+window2 = Tk()
+window2.title("Welcome to the acanacar app")
+# set window size
+window2.geometry('500x200')
+
+selected = IntVar()
+rad1 = Radiobutton(window2, text='First', value=1, variable=selected)
+rad2 = Radiobutton(window2, text='Second', value=2, variable=selected)
+rad3 = Radiobutton(window2, text='Third', value=3, variable=selected)
+
+
+def clicked2():
+    print(selected.get())
+    txt.insert(INSERT, 'You text goes here')
+
+
+btn = Button(window2, text="Click mee", command=clicked2())
 rad1.grid(column=0, row=2)
 rad2.grid(column=1, row=2)
 rad3.grid(column=2, row=2)
+btn.grid(column=3, row=0)
+window2.mainloop()
 
-window.mainloop()
+# ScrolledText widget - textarea
 
-window.mainloop()
+from tkinter import scrolledtext
+
+window2 = Tk()
+window2.title("Welcome to the acanacar app")
+# set window size
+window2.geometry('500x200')
+
+txt = scrolledtext.ScrolledText(window2, width=40, height=10)
+txt.grid(column=0, row=0)
+
+
+def clicked2():
+    txt.insert(INSERT, 'You text goes here')
+
+
+btn = Button(window2, text="Click mee", command=clicked2)
+btn.grid(column=1, row=0)
+
+window2.mainloop()
