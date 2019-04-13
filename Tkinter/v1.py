9d6982369257498e27e59a901bcce72e5f23a847
acanacar <ahmetcanacar.1@gmail.com>
2019-04-13 21:26:38 +0300

    radio and check button is added

--- Tkinter/v1.py
+++ Tkinter/v1.py
@@ -3,7 +3,7 @@ from tkinter import *
 window = Tk()
 window.title("Welcome to the acanacar app")
 # set window size
-window.geometry('350x200')
+window.geometry('500x200')
 
 lbl = Label(window, text="Hi")
 lbl.grid(column=0, row=0)
@@ -17,6 +17,7 @@ txt = Entry(
 )
 txt.grid(column=1, row=0)
 
+
 def clicked():
     res = 'Welcome to ' + txt.get()
     lbl.configure(text=res)
@@ -25,8 +26,32 @@ def clicked():
 btn = Button(window, text='Click Me', command=clicked)
 btn.grid(column=2, row=0)
 
+# combobox widget
 from tkinter.ttk import *
 
 combo = Combobox(window)
+combo['values'] = (1, 2, 3, 4, 5, 'Text')
+combo.current(1)
+combo.grid(column=0, row=1)
+
+# check button
+chk_state = BooleanVar()
+chk_state.set(True)  # set check state
+# chk_state.set(1)  # set check state
+# chk_state.set(0)  # set check state
+chk = Checkbutton(window, text='Choose')
+chk.grid(column=1, row=1)
+
+# radio buttons widgets
+
+rad1 = Radiobutton(window, text='First', value=1)
+rad2 = Radiobutton(window, text='Second', value=2)
+rad3 = Radiobutton(window, text='Third', value=3)
+
+rad1.grid(column=0, row=2)
+rad2.grid(column=1, row=2)
+rad3.grid(column=2, row=2)
+
+window.mainloop()
 
 window.mainloop()
